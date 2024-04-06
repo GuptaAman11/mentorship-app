e80621bc2e00b4b662e5b2edbe5851e6e9ce1036
shubham <shubham@shubhams-MacBook-Air.local>
2024-04-07 00:55:29 +0530

    done

--- frontend/src/component/MenteeCard/MenteesCard.jsx
+++ frontend/src/component/MenteeCard/MenteesCard.jsx
@@ -9,21 +9,34 @@ const MenteesCard = () => {
     getAllUser();
   }, []);
   const navigate = useNavigate();
-  const mentor = allUser.filter((user) => user.typeOfUser === "mentor");
-  console.log(mentor);
+  
 
-  const mentees = allUser.filter((user) => user.typeOfUser === "mentees");
-  console.log(mentees);
-
+  const mentees = allUser.filter((user) => user.typeOfUser === "mentee");
+console.log(mentees)
   return (
-    <div className="menteecard-main-div">
-      {allUser.map((user, index) => (
+    <>
+    {
+      mentees.length===0 ? (<div><h1>no mentees found</h1></div>) : (<div className="menteecard-main-div">
+      {mentees.map((user, index) => (
         <div key={index} className="meenteecard-container">
           <div className="image">
             <img src={`http://localhost:8000/${user.image}`} alt="" />
           </div>
           <div className="mentee-name">{user.name}</div>
-          <div className="bio">software developer | data analyst</div>
+
+          <div className="m-qualification">Qualification : <span>{user.qualification}</span></div>
+
+          <div className="m-interest">
+            <div className="m-interest-1">Interest :</div>
+            <div className="m-interest-2">{user.areaOfInterest}</div>
+          </div>
+          <hr />
+
+
+          <div className="m-bio"> 
+            <div className="m-goal-1">Goal : </div>
+            <div className="m-goal-2">{user.goal}</div>
+          </div>
           <div className="message-button">
             <button
               onClick={() => {
@@ -35,7 +48,9 @@ const MenteesCard = () => {
           </div>
         </div>
       ))}
-    </div>
+    </div>)
+    }
+    </>
 
   );
 };
--- frontend/src/component/MenteeCard/MentorCard.jsx
+++ frontend/src/component/MenteeCard/MentorCard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import   './menteecard.css';
 import { useGetAllUser } from '../../hooks/menteesHooks';
+import { Link, useNavigate } from "react-router-dom";
+
 
 const MentorCard = () => {
   const {getAllUser ,allUser} = useGetAllUser()
@@ -8,6 +10,7 @@ const MentorCard = () => {
     getAllUser()
   },[])
 
+  const navigate = useNavigate()
 
   const mentor  = allUser.filter(user => user.typeOfUser==='mentor')
   console.log(mentor)
@@ -21,14 +24,35 @@ const MentorCard = () => {
     { mentor.map((user ,index) =>(
        <div key={index} className="meenteecard-container">
      <div className="image">
-       <img src={`http://localhost:8000/${user.image}`} alt="" />
-     </div>
-     <div className="mentee-name">{user.name}</div>
-     <div className="bio">software developer | data analyst</div>
-     <div className="message-button"><button>message</button></div>
-   </div>
-     ))}
-  </div>)
+            <img src={`http://localhost:8000/${user.image}`} alt="" />
+          </div>
+          <div className="mentee-name">{user.name}</div>
+
+          <div className="m-qualification">Qualification : <span>{user.qualification}</span></div>
+
+          <div className="m-interest">
+            <div className="m-interest-1">Interest :</div>
+            <div className="m-interest-2">{user.areaOfInterest}</div>
+          </div>
+          <hr />
+
+
+          <div className="m-bio"> 
+            <div className="m-goal-1">Goal : </div>
+            <div className="m-goal-2">{user.goal}</div>
+          </div>
+          <div className="message-button">
+            <button
+              onClick={() => {
+                navigate(`/messages/${user._id}`);
+              }}
+            >
+              message
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>)
    }
    </>
   );
